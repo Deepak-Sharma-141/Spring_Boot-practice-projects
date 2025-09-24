87f35b131f97e6f0c588aeddf7f0512fb19cea04
Deepak-Sharma-141 <dsharma19963006@gmail.com>
2025-09-24 18:59:37 +0530

    project on Spring Data JPA

---
+++ hospitalManagement/hospitalManagement/src/main/java/com/practice/springboot/hospitalManagement/entity/Patient.java
@@ -0,0 +1,34 @@
+package com.practice.springboot.hospitalManagement.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Entity
+
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    private LocalDate birthDate;
+    private String email;
+    private String gender;
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                ", email='" + email + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
---
+++ hospitalManagement/hospitalManagement/src/test/java/com/practice/springboot/hospitalManagement/HospitalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springboot.hospitalManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
